985402fa4ba2f2cfd52596d77ffad19b0d65d96b
Adam <smitha@fullcount.net>
2023-12-05 13:42:32 -0600

    IDK PWA?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const withPWA = require('next-pwa')({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  });
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
+    experimental: {
+      newNextLinkBehavior: true,
+    },
     output: 'export',
-}
-
+  });
 module.exports = nextConfig
