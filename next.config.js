4e07334c9e5c6e874206b7b2e19de69d183c8817
Adam <smitha@fullcount.net>
2023-12-05 13:58:08 -0600

    Fixing good app

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const withPWA = require('next-pwa')({
     dest: "public",
     register: true,
-    skipWaiting: true,
+    skipWaiting: true
   });
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
-
     output: 'export',
   });
 module.exports = nextConfig
