abfc683cde1715a9c0b9cb05424c6b72fb7dc362
Adam <smitha@fullcount.net>
2023-12-05 13:20:30 -0600

    Update CoffeCounter.tsx

--- src/app/CoffeCounter.tsx
+++ src/app/CoffeCounter.tsx
@@ -91,16 +91,19 @@ const useLocalStorage = (
 	{ serialize = JSON.stringify, deserialize = JSON.parse } = {}
 ) => {
 	const [state, setState] = React.useState(() => {
-		const valueInLocalStorage = window.localStorage.getItem(key);
-		if (valueInLocalStorage) {
-			// the try/catch is here in case the localStorage value was set before
-			// we had the serialization in place (like we do in previous extra credits)
-			try {
-				return deserialize(valueInLocalStorage);
-			} catch (error) {
-				window.localStorage.removeItem(key);
-			}
-		}
+        if (typeof window !== "undefined") {
+            // Client-side-only code
+            const valueInLocalStorage = window.localStorage.getItem(key);
+            if (valueInLocalStorage) {
+                // the try/catch is here in case the localStorage value was set before
+                // we had the serialization in place (like we do in previous extra credits)
+                try {
+                    return deserialize(valueInLocalStorage);
+                } catch (error) {
+                    window.localStorage.removeItem(key);
+                }
+            }
+          }
 		return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
 	});
 
