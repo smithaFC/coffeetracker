c2b8606ce5a719328e5c02ef0ae06029c851c50e
Adam <smitha@fullcount.net>
2023-12-05 14:24:16 -0600

    idk what im doing

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,27 +12,27 @@ export default function manifest(): MetadataRoute.Manifest {
     "scope": ".",
     "icons": [
         {
-          "src": "favicon.ico",
+          "src": "./favicon.ico",
           "sizes": "32x32",
           "type": "image/x-icon"
         },
         {
-          "src": "icon-192.png",
+          "src": "./icon-192.png",
           "type": "image/png",
           "sizes": "192x192"
         },
         {
-          "src": "icon-192-maskable.png",
+          "src": "./icon-192-maskable.png",
           "type": "image/png",
           "sizes": "192x192"
         },
         {
-          "src": "icon-512.png",
+          "src": "./icon-512.png",
           "type": "image/png",
           "sizes": "512x512"
         },
         {
-          "src": "icon-512-maskable.png",
+          "src": "./icon-512-maskable.png",
           "type": "image/png",
           "sizes": "512x512"
         }
