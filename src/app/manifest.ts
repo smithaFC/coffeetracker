6dc891cde3029f6aa32c068cca4b0bed8d1448bf
Adam <smitha@fullcount.net>
2023-12-11 07:35:25 -0600

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,41 +1,41 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from 'next';
+
 export default function manifest(): MetadataRoute.Manifest {
-  return {
-    "short_name": "FC Coffee",
-    "name": "FullCount Coffee Tracker",
-    "start_url": "/smithaFC.github.io/",
-    "display": "standalone",
-    "theme_color": "#565296",
-    "background_color": "#ffffff",
-    "id": "/smithaFC.github.io/",
-    "scope": ".",
-    "icons": [
-        {
-          "src": "favicon.ico",
-          "sizes": "32x32",
-          "type": "image/x-icon"
-        },
-        {
-          "src": "icon-192.png",
-          "type": "image/png",
-          "sizes": "192x192"
-        },
-        {
-          "src": "icon-192-maskable.png",
-          "type": "image/png",
-          "sizes": "192x192"
-        },
-        {
-          "src": "icon-512.png",
-          "type": "image/png",
-          "sizes": "512x512"
-        },
-        {
-          "src": "icon-512-maskable.png",
-          "type": "image/png",
-          "sizes": "512x512"
-        }
-      ],
-  }
+	return {
+		short_name: 'FC Coffee',
+		name: 'FullCount Coffee Tracker',
+		start_url: '/smithaFC.github.io/home',
+		display: 'standalone',
+		theme_color: '#565296',
+		background_color: '#ffffff',
+		id: '/smithaFC.github.io/',
+		scope: '.',
+		icons: [
+			{
+				src: 'favicon.ico',
+				sizes: '32x32',
+				type: 'image/x-icon',
+			},
+			{
+				src: 'icon-192.png',
+				type: 'image/png',
+				sizes: '192x192',
+			},
+			{
+				src: 'icon-192-maskable.png',
+				type: 'image/png',
+				sizes: '192x192',
+			},
+			{
+				src: 'icon-512.png',
+				type: 'image/png',
+				sizes: '512x512',
+			},
+			{
+				src: 'icon-512-maskable.png',
+				type: 'image/png',
+				sizes: '512x512',
+			},
+		],
+	};
 }
